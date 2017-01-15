de096fbb8745432214da9edf6a91dcec0280c3dc
German Gladkov <ggladkov@e1z1r2p10.42.us.org>
2017-01-15 10:54:43 -0800

    randomg files from day 01-04

---
+++ temp/d02old/ex00/main.c
@@ -0,0 +1,8 @@
+#include "ft_print_alphabet.c"
+
+void ft_print_alphabet();
+int main()
+{
+	ft_print_alphabet();
+	return (0);
+}
---
+++ temp/d02old/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggladkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/11 17:51:08 by ggladkov          #+#    #+#             */
+/*   Updated: 2017/01/11 22:56:00 by ggladkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char ft_print_reverse_alphabet(int)
+char ft_print(int);
+int ft_putchar(char c):
+int main()
+{
+	char 'z';
+
+	while (i =< 
+			}
+
+ 
+
