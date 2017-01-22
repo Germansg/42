6eb1ab5c4abbfbbdb0d81b961d7b602360699b04
German Gladkov <ggladkov@e1z1r3p19.42.us.org>
2017-01-21 22:02:34 -0800

    rush 00

---
+++ bonusrush/rush00.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggladkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/14 14:03:32 by ggladkov          #+#    #+#             */
+/*   Updated: 2017/01/15 21:10:58 by ggladkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_putchar(char c);
+
+int	rush(int x, int y)
+{
+	int a;
+	int b;
+
+	a = 1;
+	while (a <= y)
+	{
+		b = 1;
+		while (b <= x)
+		{
+			if ((b == 1 || b == x) && a == 1)
+				ft_putchar('o');
+			else if ((b == 1 || b == x) && a == y)
+				ft_putchar('o');
+			else if (a == 1 || a == x)
+				ft_putchar('-');
+			else if (b == 1 || b == y)
+				ft_putchar('|');
+			else
+				ft_putchar(' ');
+			b++;
+		}
+		ft_putchar('\n');
+		a++;
+	}
+	return (0);
+}
